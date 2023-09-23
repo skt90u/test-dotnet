5a10e7d587f7a1199c4c6cfbb7f983dbbdc1f1d3
skt90u <skt90u@gmail.com>
2023-09-23 17:43:38 +0800

    *

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    internal class Program
+    internal partial class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine(ToMD5("1234"));
+                Console.WriteLine(ToMD5("1234AABBCC"));
+                //TestAutoFac(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public static string ToMD5(string str)
+        {
+            using (var cryptoMD5 = System.Security.Cryptography.MD5.Create())
+            {
+                //將字串編碼成 UTF8 位元組陣列
+                var bytes = Encoding.UTF8.GetBytes(str);
+
+                //取得雜湊值位元組陣列
+                var hash = cryptoMD5.ComputeHash(bytes);
+
+                //取得 MD5
+                var md5 = BitConverter.ToString(hash)
+                  .ToUpper();
+
+                return md5;
+            }
         }
     }
 }
---
+++ ConsoleApp/Program_AutoFac.cs
@@ -0,0 +1,52 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class A
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("[TestAutoFac] " + GetType().Name + " Hi");
+        }
+    }
+
+    class B
+    {
+        A a;
+
+        public B(A a)
+        {
+            this.a = a;
+        }
+
+        public void SayHi()
+        {
+            a.SayHi();
+            Console.WriteLine("[TestAutoFac] " + GetType().Name + " Hi");
+        }
+    }
+
+    internal partial class Program
+    {
+        static void TestAutoFac(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            // 順序可顛倒，但是相依的都必須要註冊到
+            builder.RegisterType<B>();
+            builder.RegisterType<A>();
+
+            var container = builder.Build();
+
+            var b = container.Resolve<B>();
+
+            b.SayHi();
+        }
+    }
+}
