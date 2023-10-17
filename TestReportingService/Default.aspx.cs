ebb15c7ce95aa106632d1f0c570f697a9dbad0cf
skt90u <skt90u@gmail.com>
2023-10-17 22:59:31 +0800

    TestReportingService 在 asp.net 使用 business model 作為 rdlc 資料來源

--- TestReportingService/Default.aspx.cs
+++ TestReportingService/Default.aspx.cs
@@ -14,10 +14,42 @@ namespace TestReportingService
         {
             if (!IsPostBack)
             {
-                ReportViewer1.LocalReport.SetParameters(new List<ReportParameter> { 
-                    new ReportParameter(name: "ReportParameter1", value: "SSS")
-                });
-                ReportViewer1.LocalReport.Refresh();
+                //ReportViewer1.LocalReport.SetParameters(new List<ReportParameter> { 
+                //    new ReportParameter(name: "ReportParameter1", value: "SSS")
+                //});
+
+                var details = new List<ReportData>();
+
+                for(var i=0; i<10; i++)
+                {
+                    var detail = new ReportData {
+                        N01 = i + 1,
+                        N02 = i + 2,
+                        N03 = i + 3,
+                        N04 = i + 4,
+                        N05 = i + 5,
+                        N06 = i + 6,
+                        N07 = i + 7,
+                        N08 = i + 8,
+                        N09 = i + 9,
+                        N10 = i + 10,
+                        S01 = (i + 1).ToString(),
+                        S02 = (i + 2).ToString(),
+                        S03 = (i + 3).ToString(),
+                        S04 = (i + 4).ToString(),
+                        S05 = (i + 5).ToString(),
+                        S06 = (i + 6).ToString(),
+                        S07 = (i + 7).ToString(),
+                        S08 = (i + 8).ToString(),
+                        S09 = (i + 9).ToString(),
+                        S10 = (i + 10).ToString(),
+                    };
+
+                    details.Add(detail);
+                }
+                ReportViewer1.LocalReport.DataSources.Add(new ReportDataSource("Details", details));
+
+                //ReportViewer1.LocalReport.Refresh();
             }
             
         }
---
+++ TestReportingService/Reports/ReportData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public class ReportData
+{
+    /// <summary>
+    /// 用於設定可用資料集
+    /// 
+    /// https://support.syncfusion.com/kb/article/7919/how-to-create-rdlc-report-using-business-object-data-source
+    /// </summary>
+    public List<ReportData> Example()
+    {
+        return new List<ReportData>();
+    }
+
+    public int N01 { get; set; }
+    public int N02 { get; set; }
+    public int N03 { get; set; }
+    public int N04 { get; set; }
+    public int N05 { get; set; }
+    public int N06 { get; set; }
+    public int N07 { get; set; }
+    public int N08 { get; set; }
+    public int N09 { get; set; }
+    public int N10 { get; set; }
+
+    public string S01 { get; set; }
+    public string S02 { get; set; }
+    public string S03 { get; set; }
+    public string S04 { get; set; }
+    public string S05 { get; set; }
+    public string S06 { get; set; }
+    public string S07 { get; set; }
+    public string S08 { get; set; }
+    public string S09 { get; set; }
+    public string S10 { get; set; }
+}
